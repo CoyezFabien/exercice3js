5231cd75ac1973cb869a989855114cc8808ad71c
CoyezFabien <coyez.fabien@gmail.com>
2020-11-06 17:53:31 +0100

    exercice3js

---
+++ Assets/JS/ex3.js
@@ -0,0 +1,5 @@
+
+var name = prompt('Quel est ton nom?')
+alert('Bonjour' + name + '!') 
+// var pour indiquer que l'on va faire une var nomé "name" qui est égale à prompte
+// prompt (''); une différence avec l'alerte qui permet d'avoir un petit bloc de texte ou l'utilisateur va pouvoir entrer quelque chose
